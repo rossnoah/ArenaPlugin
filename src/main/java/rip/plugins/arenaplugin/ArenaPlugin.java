2bd55e9ac45536c5a472747b46f3409e4b1e10ab
Noah Ross <me@noah.dev>
2024-01-11 08:36:16 +0100

    Initial commit

---
+++ src/main/java/rip/plugins/arenaplugin/ArenaPlugin.java
@@ -0,0 +1,17 @@
+package rip.plugins.arenaplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ArenaPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
